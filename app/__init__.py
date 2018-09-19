bafaefb63b046b57900197cecbfc699a5e0eba06
Prince Mathur <princemathur.mathur4@gmail.com>
2018-09-19 06:29:26 +0530

    Chapter4

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)   #instance
-from app import routes
+app.config.from_object(Config)
+
+db= SQLAlchemy(app)
+migrate= Migrate(app,db)
+
+# from app import routes
+# from app import routes1_1
+# from app import routes2_1
+# from app import routes2_2
+from app import routes2_3,models
+
+if(__name__=='__main__'):
+    app.run()
+    
